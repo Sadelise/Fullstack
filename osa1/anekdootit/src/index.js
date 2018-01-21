e3a209576da341e7831124456d9d25d54f7139ac
Sadelise <sharhio@hotmail.com>
2018-01-21 21:12:49 +0200

    1.13

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -5,13 +5,52 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            selected: 0
+            selected: 0,
+            anecdotes: [
+                {
+                    text: 'If it hurts, do it more often',
+                    votes: 0
+                },
+                {
+                    text: 'Adding manpower to a late software project makes it later!',
+                    votes: 0
+                },
+                {
+                    text: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+                    votes: 0
+                },
+                {
+                    text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+                    votes: 0
+                },
+                {
+                    text: 'Premature optimization is the root of all evil.',
+                    votes: 0
+                },
+                {
+                    text: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+                    votes: 0
+                },
+            ]
+        }
+    }
+
+    vote = () => {
+        let changedAnecdotes = this.state.anecdotes
+        const votes = this.state.anecdotes[this.state.selected]["votes"]
+        changedAnecdotes[this.state.selected].votes = votes + 1
+
+        console.log(votes)
+
+        return () => {
+            this.setState({
+                anecdotes: changedAnecdotes
+            })
         }
     }
 
     next = () => {
         const next = Math.floor(Math.random() * 6);
-        console.log("nextissä " + next)
         return () => {
             this.setState(() => ({
                 selected: next
@@ -22,31 +61,31 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <p>{this.props.anecdotes[this.state.selected]}</p>
-                <Button next={this.next} />
+                <p>{this.state.anecdotes[this.state.selected]["text"]}</p>
+                <Button toiminto={this.next()}
+                    otsikko="next anecdote" />
+                <Button toiminto={this.vote()}
+                    otsikko="vote" />
             </div>
         )
     }
 }
 
-const Button = ({ next }) => {
+const Button = ({ toiminto, otsikko }) => {
     return (
-        <button onClick={next()}>
-            next anecdote
-           </button>
+        <button onClick={toiminto}>
+            {otsikko}
+        </button>
     )
 }
 
-const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 ReactDOM.render(
-    <App anecdotes={anecdotes} />,
+
+    // <App anecdotes={anecdotes} />,
+    <App />,
     document.getElementById('root')
 )
+
+
+
+
