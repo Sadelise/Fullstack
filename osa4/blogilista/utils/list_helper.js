5eb41fc4def3d7823b7e0e650aec61a3fde36351
Sadelise <sharhio@hotmail.com>
2018-02-11 17:38:38 +0200

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.reduce(function(sum, blog) {
+    return sum + blog.likes
+  }, 0)
+  return blogs.length === 0 ? 0 : likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
