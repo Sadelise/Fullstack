44b05156dc2e34f22b3822edd13157c4ee52d38b
Sadelise <sharhio@hotmail.com>
2018-02-21 15:33:09 +0200

    5.1

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blogs: [],
+      username: '',
+      password: '',
+      user: null,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    blogService.getAll().then(blogs =>
+      this.setState({ blogs })
+    )
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+      this.setState({ username: '', password: '', user })
+    } catch (exception) {
+      this.setState({
+        error: 'käyttäjätunnus tai salasana virheellinen',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+    const loginForm = () => (
+      <div>
+        <h2>Kirjaudu</h2>
+    
+        <form onSubmit={this.login}>
+          <div>
+            käyttäjätunnus
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            salasana
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button type="submit">kirjaudu</button>
+        </form>
+      </div>
+    )
+    
+    const Notification = ({ message }) => {
+      if (message === null) {
+        return null
+      }
+      return (
+        <div className="error">
+          {message}
+        </div>
+      )
+    }
+    
+    return (
+      <div>
+        <Notification message={this.state.error} />
+
+        {this.state.user === null ?
+          loginForm() :
+          <div>
+            <p>{this.state.user.name} logged in</p>
+          </div>
+        }
+
+        <h2>blogs</h2>
+        {this.state.blogs.map(blog =>
+          <Blog key={blog._id} blog={blog} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
