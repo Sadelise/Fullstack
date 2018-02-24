ad62f7335c1253014ea5cb569f9e74cbe55b9fc7
Sadelise <sharhio@hotmail.com>
2018-02-24 14:06:44 +0200

    5.6

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -152,8 +152,10 @@ class App extends React.Component {
 
         <h2>blogs</h2>
         {this.state.blogs.map(blog =>
-          <Blog key={blog._id} blog={blog} />
-        )}
+          <Blog key={blog._id}
+            blog={blog} />
+        )
+        }
       </div>
     );
 
--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,55 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from './Togglable'
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  render() {
+    const hideWhenVisible = { display: this.state.visible ? 'none' : '' }
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+    return (
+      <div style={blogStyle}>
+        <div style={hideWhenVisible} onClick={this.toggleVisibility}>
+          <div>
+            {this.props.blog.title} {this.props.blog.author}
+          </div>
+        </div>
+        <div style={showWhenVisible} onClick={this.toggleVisibility}>
+          <div>
+            {this.props.blog.title} {this.props.blog.author}
+            <br></br>
+            {this.props.blog.url}
+            <br></br>
+            {this.props.blog.likes} likes
+            <button>like</button>
+
+            <br></br>
+            added by {this.props.blog.user != null ?
+              this.props.blog.user.name : 'unknown'
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Blog
