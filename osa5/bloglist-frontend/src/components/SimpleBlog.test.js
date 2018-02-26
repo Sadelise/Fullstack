6852926e5324fc8c18348f1a7bf7adf9993ecea3
Sadelise <sharhio@hotmail.com>
2018-02-26 20:39:44 +0200

    5.13

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -3,18 +3,44 @@ import { shallow } from 'enzyme'
 import Blog from './SimpleBlog'
 
 describe.only('<Blog />', () => {
-  it('renders content', () => {
-    const blog = {
-      title: 'Blogi',
-      author: "Jaakko",
-      likes: 2
-    }
+    it('renders content', () => {
+        const blog = {
+            title: 'Blogi',
+            author: "Jaakko",
+            likes: 2
+        }
 
-    const blogComponent = shallow(<Blog blog={blog} />)
-    const blogDiv = blogComponent.find('.blog')
-    const likesDiv = blogComponent.find('.likes')
-    expect(blogDiv.text()).toContain(blog.title)
-    expect(blogDiv.text()).toContain(blog.author)
-    expect(likesDiv.text()).toContain(blog.likes)
-  })
+        const blogComponent = shallow(<Blog blog={blog} />)
+        const blogDiv = blogComponent.find('.blog')
+        const likesDiv = blogComponent.find('.likes')
+        expect(blogDiv.text()).toContain(blog.title)
+        expect(blogDiv.text()).toContain(blog.author)
+        expect(likesDiv.text()).toContain(blog.likes)
+
+        console.log(blogDiv.debug())
+        console.log(likesDiv.debug())
+    })
+
+    it('clicking the button calls event handler once', () => {
+        const blog = {
+            title: 'Blogi',
+            author: "Jaakko",
+            likes: 2
+        }
+
+        const mockHandler = jest.fn()
+
+        const blogComponent = shallow(
+            <Blog
+                blog={blog}
+                onClick={mockHandler}
+            />
+        )
+
+        const button = blogComponent.find('button')
+        button.simulate('click')
+        button.simulate('click')
+
+        expect(mockHandler.mock.calls.length).toBe(2)
+    })
 })
