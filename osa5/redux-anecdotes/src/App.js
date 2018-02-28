ce2be1a8737d3d2dedb4179cfda380aaf599f75f
Sadelise <sharhio@hotmail.com>
2018-02-28 17:16:09 +0200

    5.19

---
+++ osa5/redux-anecdotes/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  klik = (nappi, id) => () => {
+    this.props.store.dispatch({ type: nappi, anecdoteId: id })
+  }
+
+  render() {
+    const anecdotes = this.props.store.getState()
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.map(anecdote=>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content} 
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={this.klik('VOTE', anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+        <h2>create new</h2>
+        <form>
+          <div><input /></div>
+          <button>create</button> 
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
