3f5166d78b5ef316b3ff564b0df4fbf3ed311c7a
Sadelise <sharhio@hotmail.com>
2018-03-03 12:01:44 +0200

    6.2

--- osa6/redux-anecdotes-v2/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes-v2/src/components/AnecdoteForm.js
@@ -1,13 +1,11 @@
 import React from 'react'
+import { anecdoteCreation } from './../reducers/anecdoteReducer'
 
 class AnecdoteForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
-    this
-      .props
-      .store
-      .dispatch({type: 'CREATE', content})
+    this.props.store.dispatch(anecdoteCreation(content))
 
     e.target.anecdote.value = ''
   }
@@ -16,7 +14,7 @@ class AnecdoteForm extends React.Component {
       <div>
         <h2>create new</h2>
         <form onSubmit={this.handleSubmit}>
-          <div><input name='anecdote'/></div>
+          <div><input name='anecdote' /></div>
           <button>create</button>
         </form>
       </div>
