2000f90fc504ee0ed6b83c61946e35567167e950
Sadelise <sharhio@hotmail.com>
2018-03-03 11:51:45 +0200

    6.1

--- osa6/redux-anecdotes-v2/.eslintrc.js
+++ osa6/redux-anecdotes-v2/.eslintrc.js
@@ -1,9 +1,24 @@
 module.exports = {
     "env": {
+        "browser": true,
         "es6": true,
-        "node": true
+        "jest/globals": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react", "jest"
+    ],
     "rules": {
         "indent": [
             "error",
@@ -27,15 +42,23 @@ module.exports = {
             "error", "always"
         ],
         "arrow-spacing": [
-            "error", {
-                "before": true,
-                "after": true
-            }
+            "error", { "before": true, "after": true }
         ],
-        "no-console": 0
+        "no-console": 0,
+        "react/prop-types": 0
     },
-    "parserOptions": {
-        "sourceType": "module",
-    },
-    "parser": "babel-eslint"
+    "settings": {
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use,
+            // default to "createReactClass"
+            "pragma": "React",  // Pragma to use, default to "React"
+            "version": "15.0", // React version, default to the latest React stable release
+            "flowVersion": "0.53" // Flow version
+        },
+        "propWrapperFunctions": ["forbidExtraProps"] // The names of any functions used to wrap the
+        // propTypes object, e.g. `forbidExtraProps`.
+        // If this isn't set, any propTypes wrapped in
+        // a function will be skipped.
+    }
+
 };
