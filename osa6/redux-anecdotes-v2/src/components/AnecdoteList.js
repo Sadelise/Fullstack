625ef9ada328ad4b2e57542c4bd4eece2c886417
Sadelise <sharhio@hotmail.com>
2018-03-03 16:34:15 +0200

    6.9

--- osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -2,50 +2,48 @@ import React from 'react'
 import { anecdoteVoting } from './../reducers/anecdoteReducer'
 import { setNotification, zeroNotification } from './../reducers/notificationReducer'
 import { connect } from 'react-redux'
+import Filter from './Filter'
 
 class AnecdoteList extends React.Component {
-  handleVote = (anecdote) => {
-    this.props.anecdoteVoting(anecdote.id)
-    this.props.setNotification(`You voted '${anecdote.content}'`)
-    setTimeout(() => {
-      this.props.zeroNotification()
-    }, 5000)
-  }
-
-  anecdotesToShow = () => {
-    const { anecdotes, filter } = this.props
-    return anecdotes.filter(function(a){
-      return a.content.toLowerCase().indexOf(filter.filter) !== -1;
-    })
-  }
-
   render() {
-    const anecdotes = this.anecdotesToShow()
-
     return (
       <div>
         <h2>Anecdotes</h2>
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote => <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
+        <Filter />
+        {this.props.anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
             has {anecdote.votes}
-            <button
-              onClick={() => this.handleVote(anecdote)}>
+              <button
+                onClick={() => this.handleVote(anecdote)}>
               vote
-            </button>
-          </div>
-        </div>)}
+              </button>
+            </div>
+          </div>)}
       </div>
-    )
-  }
+    )}
+
+ handleVote = (anecdote) => {
+   this.props.anecdoteVoting(anecdote.id)
+   this.props.setNotification(`You voted '${anecdote.content}'`)
+   setTimeout(() => {
+     this.props.zeroNotification()
+   }, 5000)
+ }
+}
+
+const anecdotesToShow = (anecdotes, filter) => {
+  return anecdotes.filter(function(a){
+    return a.content.toLowerCase().indexOf(filter.filter) !== -1;
+  })
 }
 
 const mapStateToProps = (state) => {
   return {
-    anecdotes: state.anecdotes,
-    filter: state.filter
+    anecdotes: anecdotesToShow(state.anecdotes, state.filter)
   }
 }
 
@@ -55,9 +53,7 @@ const mapDispatchToProps = {
   zeroNotification
 }
 
-const ConnectedAnecdoteList = connect(
+export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(AnecdoteList)
-
-export default ConnectedAnecdoteList
